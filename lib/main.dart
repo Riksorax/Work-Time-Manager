81be2caaaa77fe85d3f7c90409816e8e9571a03d
Riksorax <Franksp20.fs@gmail.com>
2025-09-30 21:55:18 +0200

    Update to `0.2.3`, change home screen to `HomeScreen`, and refine initialization logic
    - **Version Bump**:
      - Incremented app version from `0.2.2` to `0.2.3` in `pubspec.yaml`.
    
    - **Home Screen Update**:
      - Replaced `AuthGate` with `HomeScreen` as the entry point.
    
    - **Code Cleanup**:
      - Removed redundant initialization comments for readability.
    
    - **Git Ignore Update**:
      - Excluded `.pem` files in `.gitignore`.

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,14 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:flutter_work_time/presentation/screens/auth_gate.dart';
+import 'package:flutter_work_time/presentation/screens/home_screen.dart'; // Geändert
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:intl/date_symbol_data_local.dart';
 import 'package:intl/intl.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:flutter_work_time/app_check_initializer.dart';
 
-import 'core/config/google_sign_in_config.dart'; // Import für GoogleSignInConfig hinzugefügt
+import 'core/config/google_sign_in_config.dart'; 
 import 'core/providers/providers.dart';
 import 'core/theme/app_theme.dart';
 import 'firebase_options.dart';
@@ -17,13 +17,9 @@ import 'presentation/view_models/theme_view_model.dart';
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
-  // Initialisiere die Datumsformatierung für die deutsche Sprache.
   await initializeDateFormatting('de_DE', null);
   Intl.defaultLocale = 'de_DE';
 
-  // GoogleSignIn initialisieren mit dem serverClientId
-  // Dies sollte vor der Firebase-Initialisierung oder anderen Abhängigkeiten erfolgen,
-  // die möglicherweise auf ein konfiguriertes GoogleSignIn angewiesen sind.
   await GoogleSignIn.instance.initialize(
     serverClientId: GoogleSignInConfig.serverClientId,
   );
@@ -32,7 +28,6 @@ Future<void> main() async {
     options: DefaultFirebaseOptions.currentPlatform,
   );
 
-  // App Check aktivieren (Android: Debug/Release-abhängig)
   await AppBootstrap.ensureInitializedForEnv();
 
   final prefs = await SharedPreferences.getInstance();
@@ -60,7 +55,7 @@ class MyApp extends ConsumerWidget {
       theme: AppTheme.lightTheme,
       darkTheme: AppTheme.darkTheme,
       themeMode: themeMode,
-      home: const AuthGate(),
+      home: const HomeScreen(), // Geändert von AuthGate zu HomeScreen
     );
   }
 }
